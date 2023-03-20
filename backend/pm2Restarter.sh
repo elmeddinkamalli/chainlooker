8ee9f98cc169d20a6dbd583be0d08a1faae6f594
elmeddinkamalli <elmeddinkamalli@gmail.com>
2023-03-20 08:35:33 +0400

    PM2 restarter added

--- backend/pm2Restarter.sh
+++ backend/pm2Restarter.sh
@@ -1,6 +1,15 @@
-line=$( tail -n 1 test.txt )
-possibleErrors=("Error occurred" "Another error")
+#!/bin/bash
 
-if [[ " ${possibleErrors[*]} " =~ " ${line} " ]]; then
-    echo "found"
-fi
+declare -A pm2s
+pm2s[7]="test.txt"
+pm2s[8]="test2.txt"
+possibleErrors=("Error occurred" "Another error" "Something went wrong")
+
+for key in ${!pm2s[@]}; do
+    line=$( tail -n 1 ${pm2s[${key}]} )
+    if [[ " ${possibleErrors[*]} " =~ " ${line} " ]]; then
+        $(curl -X POST -H 'Content-type: application/json' --data '{"text":"'"Error: $line \nPID: $key restarted"'"}' https://hooks.slack.com/services/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)
+    fi
+done
+
+exit 0
