2ea6cba7b00c2e5c5eb3d478db9851aeb3c0a18a
elmeddinkamalli <elmeddinkamalli@gmail.com>
2023-03-20 10:48:21 +0400

    PM2 restarter added

--- backend/pm2Restarter.sh
+++ backend/pm2Restarter.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 declare -A pm2s
-pm2s[7]="test.txt"
-pm2s[8]="test2.txt"
-possibleErrors=("Error occurred" "Another error" "Something went wrong")
+pm2s[7]="/root/.pm2/logs/Fantom-out.log"
+pm2s[8]="/root/.pm2/logs/BNB-Mainnet-out.log"
+pm2s[9]="/root/.pm2/logs/Polygon-Mainnet-out.log"
+pm2s[10]="/root/.pm2/logs/Ethereum-Mainnet-out.log"
+pm2s[11]="/root/.pm2/logs/Avalanche-Mainnet-out.log"
+possibleErrors=("Invalid JSON RPC response: {\"size\":0,\"timeout\":0}" "connect ECONNREFUSED 127.0.0.1:27017")
 
 for key in ${!pm2s[@]}; do
     line=$( tail -n 1 ${pm2s[${key}]} )
     if [[ " ${possibleErrors[*]} " =~ " ${line} " ]]; then
-        $(curl -X POST -H 'Content-type: application/json' --data '{"text":"'"Error: $line \nPID: $key restarted"'"}' https://hooks.slack.com/services/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)
+        lineStringified=$(node /root/stringfier.js "${line}")
+        $(curl -X POST -H 'Content-type: application/json' --data '{"text":"'"Error: ${lineStringified} \nPID: $key restarted"'"}' https://hooks.slack.com/services/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)
+        $(/root/.nvm/versions/node/v15.14.0/bin/pm2 restart ${key})
     fi
 done
 
